acee9236af945ce7492f5bff9a10b5e6efc3e06b
gillyongs <khs20010327@gmail.com>
2023-01-17 09:56:10 +0900

    feat: 리액트 프로젝트 생성, 0주차 과제 수행

---
+++ 김한수-workspace/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ImportConponent from './R001_ImportComponent'
+
+
+function App() {
+  return (
+    <div className="App" >
+      <span>
+        Hello, World!
+      </span>  
+    </div>
+  );
+}
+
+export default App;
