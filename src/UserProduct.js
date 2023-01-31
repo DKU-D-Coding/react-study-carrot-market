6c071e8297c59e2b56a34d6f68582b914410ace8
geon woo <qkrnslehd@naver.com>
2023-01-31 17:56:52 +0900

    docs:useEffect로 nickName에 해당하는 데이터 뿌려주기

--- src/UserProduct.js
+++ src/UserProduct.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { getUserProductList } from './api';
 import './ProductPage.css';
 import UserCard from './UserCard';
 function UserProduct({ nickName }) {
   console.log(nickName);
-  const userProductList = getUserProductList(nickName);
+  const [userProductList, setUserProductList] = useState([]);
+  const getProduct = async nickName => {
+    const data = await getUserProductList(nickName);
+    setUserProductList(data);
+  };
+  useEffect(() => {
+    getProduct(nickName);
+  }, [nickName]);
   return (
     <div className='itemObjectContainer'>
       {userProductList.map(item => (
