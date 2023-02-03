9cfe5362583beabe7422f83a58dc779c80821b98
geon woo <qkrnslehd@naver.com>
2023-02-03 14:39:13 +0900

    docs:함수 일부 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,29 @@
 import axios from 'axios';
 export const getItems = async () => {
   const response = await axios.get('http://localhost:3001/items');
-  console.log(response.data);
   return response.data;
 };
+// export const postItems = async object => {
+//   axios.post('http://localhost:3001/items', object).then(response => {
+//     console.log(response);
+//   });
+// };
+// export const deleteItem = async (id) => {
+//   axios.delete('')
+// }
 
 export const getItemBySlug = async itemSlug => {
-  const data = await getItems();
-  return data.find(item => item.slug === itemSlug);
+  console.log(itemSlug);
+  const response = await axios.get('http://localhost:3001/items');
+  const itemBySlug = response.data;
+  console.log(itemBySlug);
+  return itemBySlug.find(item => item.slug === itemSlug);
 };
 
 export const getUserProductList = async nickName => {
-  const data = await getItems();
-  const userProductList = data.filter(mockItem => mockItem.nickName === nickName);
   console.log(nickName);
+  const response = await axios.get('http://localhost:3001/items');
+  const itemList = response.data;
+  const userProductList = itemList.filter(mockItem => mockItem.nickName === nickName);
   return userProductList;
 };
