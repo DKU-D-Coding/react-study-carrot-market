247e559b053cc7fd9cbeb8b28fbcb76fbd79df1f
gaeunnlee <gaeunnnleee@gmail.com>
2023-02-08 23:27:08 +0900

    refactor: 상품, 카테고리 목록 분리

---
+++ react_project/src/data/goods.js
@@ -0,0 +1,51 @@
+import EmptyImg from '../assets/image/no_img.jpg';
+
+let cards = [
+    {
+        imgSrc: EmptyImg,
+        title: "아이폰 13 미니",
+        price: 200000,
+        goodsId: "226",
+        like: 3,
+        categoryName: "디지털기기",
+        categoryKey: "digitalDevice"
+     },
+     {
+        imgSrc: EmptyImg,
+        title: "에어프라이기",
+        price: 90000,
+        goodsId: "227",
+        like: 8,
+        categoryName: "생활가전",
+        categoryKey: "homeAppliances"
+     },
+     {
+        imgSrc: EmptyImg,
+        title: "소파",
+        price: 80000,
+        goodsId: "228",
+        like: 7,
+        categoryName: "가구/인테리어",
+        categoryKey: "furniture"
+     },
+     {
+        imgSrc: EmptyImg,
+        title: "또봇 장난감",
+        price: 10000,
+        goodsId: "229",
+        like: 6,
+        categoryName: "유아동",
+        categoryKey: "kids"
+     },
+     {
+        imgSrc: EmptyImg,
+        title: "갤럭시 S22",
+        price: 230000,
+        goodsId: "230",
+        like: 2,
+        categoryName: "디지털기기",
+        categoryKey: "digitalDevice"
+     }
+]
+
+export default cards
