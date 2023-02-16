e23026359c052d6d6f4d86ba59e29a8a3e37d037
geon woo <qkrnslehd@naver.com>
2023-02-16 14:02:27 +0900

    feat: 회원가입 페이지 구현(local storage)

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { LoginState } from '../recoil/atom';
 function SignUpPage() {
   const [formValue, setFormValue] = useState({
     email: '',
@@ -9,20 +13,58 @@ function SignUpPage() {
     phone: '',
     nickName: '',
   });
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
   const handleChange = e => {
     setFormValue({
       ...formValue,
       [e.target.name]: e.target.value,
     });
   };
+  const navigate = useNavigate();
   const handleSubmit = e => {
     e.preventDefault();
-    alert(JSON.stringify(formValue));
-    axios.post('http://103.218.158.186/api/user/signup', formValue).then(res => console.log(res));
+    alert(
+      JSON.stringify({
+        email: formValue.email,
+        pw: formValue.pw,
+        name: formValue.name,
+        phone: formValue.phone,
+        nickName: formValue.nickName,
+      })
+    );
+    axios({
+      method: 'post',
+      url: '/api/user/signup',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify(formValue),
+    })
+      .then(response => {
+        console.log(response.data.data);
+        localStorage.setItem('accessToken', response.data.data.accessToken);
+        localStorage.setItem('refreshToken', response.data.data.refreshToken);
+        localStorage.setItem('userId', response.data.data.userId);
+        localStorage.setItem('nickName', response.data.data.nickName);
+        setIsLoggedIn(true);
+        navigate('/');
+      })
+      .catch(error => {
+        console.log('An error occured:', error);
+      });
   };
+  useEffect(() => {
+    if (
+      localStorage.getItem('accessToken') &&
+      localStorage.getItem('accessToken') !== 'undefined'
+    ) {
+      navigate('/');
+    }
+  }, []);
   return (
     <div>
-      회원가입하기
+      <p>안녕하세요!</p>
+      <p>정보를 입력하고 가입해주세요.</p>
       <form id='form' onSubmit={handleSubmit}>
         <input
           type='text'
