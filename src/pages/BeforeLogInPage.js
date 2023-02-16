d0d0a29b837c50e3a72a20be4122581a5351d583
geon woo <qkrnslehd@naver.com>
2023-02-16 14:02:47 +0900

    feat: 로그인 전 페이지

--- src/pages/BeforeLogInPage.js
+++ src/pages/BeforeLogInPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import carrot from '../images/carrot.jpg';
 function BeforeLogInPage() {
-  return <div>BeforeLogInPage</div>;
+  return (
+    <div>
+      <div>
+        <img src={carrot} alt={'carrot'}></img>
+      </div>
+      <div>
+        <Link to='./signup'>
+          <button>시작하기</button>
+        </Link>
+
+        <p>
+          이미 계정이 있나요?
+          <Link to='./signin'>
+            <span>로그인</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default BeforeLogInPage;
